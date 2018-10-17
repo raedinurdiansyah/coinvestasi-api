0261335f9a172727ad05a0198e59c69972585b01
pentagonal <pentagonaldevelopment@gmail.com>
2018-10-17 23:50:31 +0700

    add Module

--- app/Component/Middleware.php
+++ app/Component/Middleware.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 
 namespace ArrayIterator\Coinvestasi\Component;
 
+use ArrayIterator\Coinvestasi\Core\ApiGroup;
 use ArrayIterator\Coinvestasi\Core\Application;
+use ArrayIterator\Coinvestasi\Core\Generator\JsonPatent;
 use ArrayIterator\Coinvestasi\Core\Hook;
 use ArrayIterator\Extension\Loader;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as R;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as S;
+use Psr\Http\Message\ServerRequestInterface;
 use Slim\Http\Environment;
 use Slim\Http\Request;
+use Slim\Http\Response;
 use Slim\Http\Uri;
 
 /**
@@ -34,10 +39,30 @@ $this->add(function (S $request, R $response, callable $next) {
         $request = $request->withUri($uri->createFromEnvironment($environment));
     }
 
-    array_map(
-        [$this['extension'], 'load'],
-        $this['extension']->getAllAvailableExtensions()
-    );
+    if (!preg_match('/^Mozilla\/[0-9](?:\.[0-9])?/', trim($request->getHeaderLine('User-Agent')))
+        // set only coinvestasi allow get from ajax
+        || $request instanceof Request && $request->isXhr() &&
+           !(
+                (
+                    isset($environment['REMOTE_ADDR']) && $environment['REMOTE_ADDR'] !== '127.0.0.1'
+                    || !isset($environment['REMOTE_ADDR'])
+                ) && preg_match('/\coinvestasi\.com/', $request->getHeaderLine('Referer'))
+           )
+    ) {
+        /**
+         * @var ApiGroup $api
+         */
+        $api = $this['api'];
+        $api->any('{params: .+}', function (ServerRequestInterface $request, ResponseInterface $r) {
+            return JsonPatent::errorCode($r, null, 400);
+        });
+    } else {
+        array_map(
+            [$this['extension'], 'load'],
+            $this['extension']->getAllAvailableExtensions()
+        );
+    }
+
     return $next(
         $request->withAddedHeader(
             'Content-Type',
