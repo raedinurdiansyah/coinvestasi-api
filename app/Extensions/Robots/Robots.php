ca42210bfe02cd57b5568b4b887752a9a12df902
pentagonal <pentagonaldevelopment@gmail.com>
2018-10-17 23:57:30 +0700

    add module robots

---
+++ app/Extensions/Robots/Robots.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace ArrayIterator\Coinvestasi\Extensions\Conferences;
+
+use ArrayIterator\Coinvestasi\Core\AddOnExtension;
+use ArrayIterator\Coinvestasi\Core\Application;
+use ArrayIterator\Coinvestasi\Core\Generator\JsonPatent;
+use ArrayIterator\Extension\ExtensionInfo;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Robots extends AddOnExtension
+{
+    /**
+     * @var string
+     */
+    protected $extensionName = 'Funds';
+
+    /**
+     * @var string
+     */
+    protected $extensionDescription = 'Funds API';
+
+    /**
+     * @param ExtensionInfo $info
+     */
+    protected function onAfterConstruct(ExtensionInfo $info)
+    {
+    }
+
+    /**
+     * @param Application $a
+     *
+     * @return mixed|void
+     */
+    public function addRouteGroup(Application $a)
+    {
+        $a->any('.txt', function (ServerRequestInterface $request, ResponseInterface $r) {
+            $r->getBody()->write(<<<ROBOTS
+User-agent: *
+Disallow: /
+
+ROBOTS
+            );
+            return $r->withHeader('Content-Type', 'text/plain;charset=utf8');
+        });
+    }
+
+    /**
+     * Prepare Construct
+     */
+    protected function prepare()
+    {
+        $this->registerObjectAutoloader();
+    }
+}
